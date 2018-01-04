feb2fdd31c7e0564799c71c186b26886f7de8913
adelsamar <adels@greenitglobe.com>
2018-01-04 12:40:51 +0200

    Change repo path

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ set -e
 python3 www.threefoldtoken.com/data/content/scripts/providers.py
 python3 www.threefoldtoken.com/data/content/scripts/community.py
 
-cp www.threefoldtoken.com/data/web/*.js ../www_threefold2.0/www.threefoldtoken.com/themes/landing/static/js
-cp www.threefoldtoken.com/data/content/community/avatars/processed_* ../www_threefold2.0/www.threefoldtoken.com/themes/landing/static/avatars
-cp www.threefoldtoken.com/data/content/*.js ../www_threefold2.0/www.threefoldtoken.com/themes/landing/static/js
-cp www.threefoldtoken.com/data/web/js/*.js ../www_threefold2.0/www.threefoldtoken.com/themes/landing/static/js
-cp www.threefoldtoken.com/data/web/*.css ../www_threefold2.0/www.threefoldtoken.com/themes/landing/static/css
+cp www.threefoldtoken.com/data/web/*.js ../www_threefold/www.threefoldtoken.com/themes/landing/static/js
+cp www.threefoldtoken.com/data/content/community/avatars/processed_* ../www_threefold/www.threefoldtoken.com/themes/landing/static/avatars
+cp www.threefoldtoken.com/data/content/*.js ../www_threefold/www.threefoldtoken.com/themes/landing/static/js
+cp www.threefoldtoken.com/data/web/js/*.js ../www_threefold/www.threefoldtoken.com/themes/landing/static/js
+cp www.threefoldtoken.com/data/web/*.css ../www_threefold/www.threefoldtoken.com/themes/landing/static/css
