c3297193c163d75a6bef9a1b62bcc9029139392a
adelsamar <adels@greenitglobe.com>
2018-02-07 16:50:44 +0200

    Update js

--- www.threefoldtoken.com/themes/landing/static/js/tech-team.js
+++ www.threefoldtoken.com/themes/landing/static/js/tech-team.js
@@ -44,26 +44,28 @@ $(function () {
                 if (added.length === details.length) {
                     break;
                 }
-                if (details[i].core == false || details[i].core == null || details[i].rank > 0) {
-                    added.push(details);
-                    var parent = $("<div>").addClass("rj-team-member");
-                    var a = $("<div>").addClass('member-photo');
-                    var img = $("<img/>").addClass('rj-team-member-photo-rollover');
-                    img.prop('src', '../avatars/' + encodeURIComponent(details[i].avatar));
-                    a.append(img);
-                    parent.append(a);
-                    var div = $("<div>").addClass("rj-team-member-info-text").css('display', 'none');
-                    var imgCol = $("<div>").addClass('col-sm-3');
-                    var dataCol = $("<div>").addClass('col-sm-9');
-                    var close = $('<div>').addClass('close-bio').text('x');
-                    div.append(imgCol);
-                    imgCol.append($("<img/>").prop("src", "../avatars/" + encodeURIComponent(details[i].avatar)));
-                    div.append(dataCol);
-                    dataCol.append($("<div>").addClass('member-name').text(details[i].name));
-                    dataCol.append($("<div>").addClass('bio-excerpt').text(details[i].description));
-                    div.append(close);
-                    parent.append(div);
-                    rjteam.append(parent);
+                if (details[i].core == false || details[i].core == null) {
+                    if (details[i].rank >= 0) {
+                        added.push(details);
+                        var parent = $("<div>").addClass("rj-team-member");
+                        var a = $("<div>").addClass('member-photo');
+                        var img = $("<img/>").addClass('rj-team-member-photo-rollover');
+                        img.prop('src', '../avatars/' + encodeURIComponent(details[i].avatar));
+                        a.append(img);
+                        parent.append(a);
+                        var div = $("<div>").addClass("rj-team-member-info-text").css('display', 'none');
+                        var imgCol = $("<div>").addClass('col-sm-3');
+                        var dataCol = $("<div>").addClass('col-sm-9');
+                        var close = $('<div>').addClass('close-bio').text('x');
+                        div.append(imgCol);
+                        imgCol.append($("<img/>").prop("src", "../avatars/" + encodeURIComponent(details[i].avatar)));
+                        div.append(dataCol);
+                        dataCol.append($("<div>").addClass('member-name').text(details[i].name));
+                        dataCol.append($("<div>").addClass('bio-excerpt').text(details[i].description));
+                        div.append(close);
+                        parent.append(div);
+                        rjteam.append(parent);
+                    }
                 }
             }
         }
