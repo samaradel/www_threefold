b418da8d12122f9c205d4ece616e4b84975b2249
adelsamar <adels@greenitglobe.com>
2018-03-22 16:01:20 +0200

    Change urls

--- www.threefoldtoken.com/themes/landing/static/js/custom.js
+++ www.threefoldtoken.com/themes/landing/static/js/custom.js
@@ -43,7 +43,7 @@
     $('#contactForm').submit(function (event) {
         $.ajax({
                 type: 'POST',
-                url: 'http://threefold2.aydo.com:4040/www_threefold',
+                url: '/www_threefold',
                 data: $('#contactForm').serialize(),
                 dataType: 'json',
                 encode: true
