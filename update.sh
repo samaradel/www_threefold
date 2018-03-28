cecdde9a3e99d74b77c51ca26181cd5662478bc0
adelsamar <adels@greenitglobe.com>
2018-03-28 13:29:54 +0200

    Remove map from script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-python3 www.threefoldtoken.com/data/content/scripts/providers.py
+#python3 www.threefoldtoken.com/data/content/scripts/providers.py
 python3 www.threefoldtoken.com/data/content/scripts/community.py
 
 cp www.threefoldtoken.com/data/web/*.js ../www_threefold/www.threefoldtoken.com/themes/landing/static/js
