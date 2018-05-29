f6d4ac30594527dbdc0d28420fc5557a214051ea
adelsamar <adels@greenitglobe.com>
2018-05-29 16:02:34 +0200

    Get old navbar back

--- www.threefoldtoken.com/themes/landing/static/js/custom.js
+++ www.threefoldtoken.com/themes/landing/static/js/custom.js
@@ -1,110 +1,94 @@
 'use strict';
+(function () {
 
-function makeTimer() {
+    function makeTimer() {
 
-    var endTime = new Date("December 1, 2017 12:00:00 EST");
-    var endTime = (Date.parse(endTime)) / 1000;
+        var endTime = new Date("December 1, 2017 12:00:00 EST");
+        var endTime = (Date.parse(endTime)) / 1000;
 
-    var now = new Date();
-    var now = (Date.parse(now) / 1000);
+        var now = new Date();
+        var now = (Date.parse(now) / 1000);
 
-    var timeLeft = endTime - now;
+        var timeLeft = endTime - now;
 
-    var days = Math.floor(timeLeft / 86400);
-    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-    if (hours < "10") {
-        hours = "0" + hours;
+        if (hours < "10") {
+            hours = "0" + hours;
+        }
+        if (minutes < "10") {
+            minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+            seconds = "0" + seconds;
+        }
+
+        $("#days").html(days + "<span>Days</span>");
+        $("#hours").html(hours + "<span>Hours</span>");
+        $("#minutes").html(minutes + "<span>Minutes</span>");
+        $("#seconds").html(seconds + "<span>Seconds</span>");
     }
-    if (minutes < "10") {
-        minutes = "0" + minutes;
-    }
-    if (seconds < "10") {
-        seconds = "0" + seconds;
-    }
 
-    $("#days").html(days + "<span>Days</span>");
-    $("#hours").html(hours + "<span>Hours</span>");
-    $("#minutes").html(minutes + "<span>Minutes</span>");
-    $("#seconds").html(seconds + "<span>Seconds</span>");
-}
+    setInterval(function () {
+        makeTimer();
+    }, 1000);
 
-setInterval(function () {
-    makeTimer();
-}, 1000);
+    $('nav a[href$="/' + location.pathname.split("/")[1] + '"]').addClass('active');
 
-$('nav a[href$="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    // Mailout
 
-// Mailout
+    $('#contactForm').submit(function (event) {
+        $.ajax({
+                type: 'POST',
+                url: '/www_threefold',
+                data: $('#contactForm').serialize(),
+                dataType: 'json',
+                encode: true
+            })
+            .done(function (data) {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
 
-$('#contactForm').submit(function (event) {
-    $.ajax({
-            type: 'POST',
-            url: '/www_threefold',
-            data: $('#contactForm').serialize(),
-            dataType: 'json',
-            encode: true
-        })
-        .done(function (data) {
-            // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                .append("</button>");
-            $('#success > .alert-success')
-                .append("<strong>Your message has been sent. </strong>");
-            $('#success > .alert-success')
-                .append('</div>');
-
-            //clear all fields
-            $('#contactForm').trigger("reset");
-        })
-        .fail(function () {
-            // Fail message
-            $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                .append("</button>");
-            $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that my mail server is not responding. Please try again later!");
-            $('#success > .alert-danger').append('</div>');
-            //clear all fields
-            $('#contactForm').trigger("reset");
-        });
-    event.preventDefault();
-});
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            })
+            .fail(function () {
+                // Fail message
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that my mail server is not responding. Please try again later!");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            });
+        event.preventDefault();
+    });
 
 
-$("a[data-toggle=\"tab\"]").click(function (e) {
-    e.preventDefault();
-    $(this).tab("show");
-});
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
 
-// video handler
-const video = document.getElementById('video');
-const menu = document.getElementById('main');
-video.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-    return false;
-});
+    // video handler
+    const video = document.getElementById('video');
+    video.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+        return false;
+    });
 
-menu.addEventListener('click', () => {
-    document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("main").style.display = 'none';
-});
-
-function closeBtn() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.display = 'block';
-
-}
-
-$(document).on("click", function (event) {
-    var $trigger = $(menu);
-    if ($trigger !== event.target && !$trigger.has(event.target).length) {
-        closeBtn();
-    }
-});
+})();
